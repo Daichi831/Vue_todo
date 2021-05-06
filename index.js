97694d9843fed994bb042053a854b89247e10b8c
kanekodaichi <knkdic.bfc@gmail.com>
2021-05-06 19:17:30 +0900

    Vue.jsでTodoリストアプリを作成

---
+++ index.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      todos: [],
+      editIndex: -1
+    },
+    mounted() {
+      if (localStorage.getItem('todos')) {
+        this.todos = JSON.parse(localStorage.todos);
+      }
+    },
+    methods: {
+        doAdd: function() {
+            const comment = this.$refs.comment
+            if (!comment.value.length) {
+                return
+            }
+            this.todos.push({ comment: comment.value })
+            comment.value = ''
+            this.doSave()
+        },
+
+        doRemove: function(index) {
+            this.todos.splice(index, 1)
+            this.doSave()
+        },
+
+        doEdit: function(index) {
+            this.editIndex = this.editIndex >= 0 ? -1 : index
+        },
+
+        doSave: function() {
+            localStorage.setItem('todos', JSON.stringify(this.todos))
+        }
+    }
+  });
